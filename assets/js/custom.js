57e4d07283bf949b9a6b15c6a6666cdf33c959fc
Zac Heisey <zheisey@gmail.com>
2019-10-29 16:58:35 -0400

    finish featured attorney profile layout and begin to modularize js markup

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+---
+---
 //== Helpers ==//
 
 /**
@@ -156,39 +158,7 @@ function renderProfile(data) {
         freeConsult = '<p><span uk-icon="icon: close; ratio: 1.5;" style="color: red;"></span> NO</p>';
       }
       // Update HTML with featured attorney data
-      profileHTML +=
-        '<h1 class="uk-article-title">' + attorney.firstName + ' ' + attorney.lastName + ' | ' + attorney.practiceName + '</h1>' +
-        '<p class="uk-text-lead uk-text-muted"><span id="featured-icon" uk-icon="icon: bookmark; ratio: 2;"></span>Featured Estate Planning Attorney in ' + attorney.city + ', ' + attorney.stateShort + '</p>' +
-        '<div class="uk-child-width-1-1@m uk-grid-match uk-margin-medium-top" data-uk-grid>' +
-          '<div class="featured-profile">' +
-            '<div id="profile-card" class="uk-card uk-card-default uk-box-shadow-medium uk-card-hover uk-card-body uk-inline border-radius-large border-xlight">' +
-              // Left side of profile card
-              '<div id="left">' +
-                '<img class="uk-border-circle" src="' + attorney.image + '" alt="' + attorney.firstName + ' ' + attorney.lastName + ' headshot">' +
-                '<address>' +
-                  '<p class="practice">' + attorney.practiceName + '</p>' +
-                  '<p class="street">' + attorney.street + '</p>' +
-                  '<p class="city-zip">' + attorney.city + ', ' + attorney.stateShort + ' ' + attorney.zip + '</p>' +
-                '</address>' +
-                '<a class="uk-button uk-button-success" href=""><span uk-icon="icon: comment;"></span>Send Message</a>' +
-              '</div>' +
-              // Right side of profile card
-              '<div id="right">' +
-                '<h3 class="uk-card-title uk-margin">Practice Areas</h3>' +
-                '<ul>' +
-                  '<li>' + attorney.practiceArea1 + '</li>' +
-                  '<li>' + attorney.practiceArea2 + '</li>' +
-                  '<li>' + attorney.practiceArea3 + '</li>' +
-                  '<li>' + attorney.practiceArea4 + '</li>' +
-                '</ul>' +
-                '<h3 class="uk-card-title uk-margin">Free Consultation?</h3>' +
-                freeConsult +
-                '<a class="uk-button uk-button-success" href=""><span uk-icon="icon: receiver;"></span>Call</a>' +
-                '<a class="uk-button uk-button-success" href=""><span uk-icon="icon: link;"></span>Visit Website</a>' +
-              '</div>' +
-            '</div>' +
-          '</div>' +
-        '</div>';
+      profileHTML += {% include featured-profile-markup.js %};
     }
 
   });
